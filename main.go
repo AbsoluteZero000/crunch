35312931e9776e68d1c31ea9bb5d8ac1f42b2a6f
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-09-27 16:11:02 +0300

    feat: file io

--- main.go
+++ main.go
@@ -3,7 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 )
+
+type Pair struct {
+	Char  rune
+	Count int
+}
+
+func getSortedFrequency(s string) []Pair {
+	frequencyMap := make(map[rune]int)
+
+	for _, char := range s {
+		frequencyMap[char]++
+	}
+
+	pairs := make([]Pair, 0, len(frequencyMap))
+	for char, count := range frequencyMap {
+		pairs = append(pairs, Pair{Char: char, Count: count})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		if pairs[i].Count == pairs[j].Count {
+			return pairs[i].Char < pairs[j].Char
+		}
+		return pairs[i].Count > pairs[j].Count
+	})
+
+	return pairs
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -18,4 +46,13 @@ func main() {
 	}
 
 	fmt.Println(string(data))
+
+	sorted_slice := getSortedFrequency(string(data))
+
+	fmt.Println(sorted_slice)
+
+	for _, pair := range sorted_slice {
+		fmt.Printf("Character: '%c', Frequency: %d\n", pair.Char, pair.Count)
+	}
+
 }
